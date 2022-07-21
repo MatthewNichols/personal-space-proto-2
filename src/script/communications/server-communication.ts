b57051a93ee691a28dc9ac989683d42a7f59c665
Matthew Nichols <matthew@matthew-nichols.com>
2022-07-21 18:28:07 +0200

    Setting up initial server connection ability.

--- src/script/communications/server-communication.ts
+++ src/script/communications/server-communication.ts
@@ -11,3 +11,26 @@ import { io, Socket } from "socket.io-client";
  * 
  * Very Important that the Clients know which avatar is themselves and what is others
  */
+
+let instance: ServerConnection | null;
+
+export const getServerConnection = (): ServerConnection => {
+    if (!instance) {
+        instance = new ServerConnection(import.meta.env.VITE_API_URL, import.meta.env.VITE_WS_URL);
+    }
+
+    return instance;
+}
+
+class ServerConnection {
+    socket: Socket;
+
+    constructor(private apiUrl: string, wsUrl: string) {
+        if (wsUrl) {
+            this.socket = io(wsUrl);
+        } else {
+            this.socket = io();
+        }
+    }
+
+}
